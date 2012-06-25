ce5c74db29a68ca99522eeb87b0b475fd04bf315
Matteo Landi <matteo@matteolandi.net>
2012-06-25 07:38:46 +0200

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup
 
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 NAME = 'logfilter'
 MODULES = [NAME]
 DESCRIPTION = 'Poll log files for updates and highlight words based on regexp filters'

8a89b072a7c7480772b3d84e0990b25abb078f1f
Matteo Landi <matteo@matteolandi.net>
2012-08-10 07:56:01 +0200

    Bump version to 0.3.1dev.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-VERSION = '0.2.1'
+VERSION = '0.3.1'
 NAME = 'logfilter'
 PACKAGES = [NAME]
 DESCRIPTION = 'Poll log files for updates and highlight words based on regexp filters'

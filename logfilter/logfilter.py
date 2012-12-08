78b12191708cc01bd417f836676be57a9ed019fa
Matteo Landi <matteo@matteolandi.net>
2012-12-08 15:24:23 +0100

    Fix syntax errors for py3k

--- logfilter/logfilter.py
+++ logfilter/logfilter.py
@@ -625,10 +625,10 @@ class AutoScrollbar(tkinter.Scrollbar):
         tkinter.Scrollbar.set(self, lo, hi)
 
     def pack(self, **kw):
-        raise tkinter.TclError, "cannot use pack with this widget"
+        raise tkinter.TclError("cannot use pack with this widget")
 
     def place(self, **kw):
-        raise tkinter.TclError, "cannot use place with this widget"
+        raise tkinter.TclError("cannot use place with this widget")
 
 
 @debug
